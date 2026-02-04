6efe7f599d30243ed78eb2036bc999c93bca5f2f
Kavya9999 <kavyavankayalapati9999@gmail.com>
2026-02-04 12:55:42 +0530

    Updated code

---
+++ control statements.py/simpleif.py
@@ -0,0 +1,14 @@
+"""Write a program to print a number if it is even"""
+#n=int(input('enter the number:'))
+#if n%2==0:
+#    print(n)
+
+"""Write a program to print the square of a number if number is even"""
+#n=int(input('enter the value'))
+#if n%2==0:
+#     print(n**2)
+
+"""Write a program whether the character is vowel"""
+#char=(input('enter the char:'))
+#if char  in ['a','e','i','o','u']:
+#    print(char)
