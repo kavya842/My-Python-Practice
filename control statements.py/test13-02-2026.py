81fd2c99c70ffb6f62949c1a308ad3ef578b0988
kavya842 <kavyavankayalapati9999@gmail.com>
2026-02-15 11:42:20 +0530

    Add while loop and update test files

--- control statements.py/test13-02-2026.py
+++ control statements.py/test13-02-2026.py
@@ -17,8 +17,34 @@
 # else:
 #     print('perform division operation',n1/n2)
 """Write  a program to check whether a string is palindrome or not"""
-ch=(input('enter the char:'))
-if ch==ch[::-1]:
-    print('it is a palindrome')
-else:
-    print('it is not a  palindrome')
+# ch=(input('enter the char:'))
+# if ch==ch[::-1]:
+#     print('it is a palindrome')
+# else:
+#     print('it is not a  palindrome')
+# """Write  a program to print helloworld for 20 times without using loops concept """
+# print('helloworld')
+# print('helloworld')
+# print('helloworld')
+# print('helloworld')
+# print('helloworld')
+# print('helloworld')
+# print('helloworld')
+# print('helloworld')
+# print('helloworld')
+# print('helloworld')
+# print('helloworld')
+# print('helloworld')
+# print('helloworld')
+
+# print('helloworld')
+# print('helloworld')
+# print('helloworld')
+# print('helloworld')
+# print('helloworld')
+# print('helloworld')
+# print('helloworld')
+"""Write a program to perform addition between two numbers"""
+# a=int(input())
+# b=int(input())
+# print(a+b)
---
+++ loopingstatements.py/whileloop.py
@@ -0,0 +1,31 @@
+"""Write a program to print 10 to 1 numbers"""
+# i=10
+# while i>=1:
+#     print(i)
+#     i-=1
+"""Write a program to print 1 to n """
+# n=int(input('enter the number:'))
+# i=1
+# while i<=n:
+#     print(i)
+#     i+=1
+"""Write a program to print even numbers from 1 to n"""
+# n=int(input('enter the number:'))
+# i=1
+# while (i<=n):
+#     if i%2==0:
+#       print(i)
+#     i+=1
+"""Write a program to print multiplication table of a digit"""
+# n=int(input('enter the digit:'))
+# i=1
+# while (i<=10):
+#      print(f'{n}*{i}={i*n}')
+#      i+=1
+"""Write a  program to print factors of a number"""
+# n=int(input('enter the digit:'))
+# i=1
+# while(i<=n):
+#     if n%i==0:
+#       print(i)
+#     i+=1
