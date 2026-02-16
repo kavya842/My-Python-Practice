c0e520a13fe2362ef8ae67356c28e3372f5ef7a2
kavya842 <kavyavankayalapati9999@gmail.com>
2026-02-16 21:52:33 +0530

    Updated control statements and looping statements practice code

--- control statements.py/simpleif.py
+++ control statements.py/simpleif.py
@@ -1,14 +1,14 @@
-# """Write a program to print a number if it is even"""
+"""Write a program to print a number if it is even"""
 # n=int(input('enter the number:'))
 # if n%2==0:
 #     print(n)
 
-# """Write a program to print the square of a number if number is even"""
+"""Write a program to print the square of a number if number is even"""
 # n=int(input('enter the value'))
 # if n%2==0:
 #      print(n**2)
 
-# """Write a program whether the character is vowel"""
+"""Write a program whether the character is vowel"""
 # char=(input('enter the char:'))
 # if char  in ['a','e','i','o','u']:
 #     print(char)
@@ -18,7 +18,7 @@
 # if char in ('a','e','i','o','u','A','E','I','O','U'):
 #     print('it is a vowel')
 
-# """Write a program to print the ascii value of a char  if char is uppercase"""
+"""Write a program to print the ascii value of a char  if char is uppercase"""
 # char=input('enter the character:')
 # if 'A' <= char <= 'Z':
 #     print('the ascii value of character:',ord(char))
@@ -27,13 +27,13 @@
 # if 'A'<=ch<='Z':
 #    print(ord(ch))
 
-# """Write a program to print cube of a number if it is divisible by 9 and 6"""
+"""Write a program to print cube of a number if it is divisible by 9 and 6"""
 # a=int(input('enter the number:'))
 # if a %9 == 0 and a %6 == 0:
 #     print('the cube number:',a**3)
 
 # "Today Assignment 04-02-2026"
-# #1)Write a program to print last digit of a num if it is 2 digit number
+# 1)Write a program to print last digit of a num if it is 2 digit number
 # num=int(input('enter the number:'))
 # if 10<=abs(num)<=99:
 #    print(abs(num)%10)
@@ -50,14 +50,14 @@
 #            #example 45%10=5
 #            #print()-->prints the last digit
 
-# """ Write to check whether a last digit of a num is divisible by 2"""
+""" Write to check whether a last digit of a num is divisible by 2"""
 # num=int(input('enter the number:'))
 # if (abs(num)%10)%2==0:
 #        print("Divisible by 2")
 
 #           #output: 38 % 10 =8 (last_digit) % 2==0 checks if it is even , even number --> divisible by 2 , if condition is true -->prints message
 
-# """ write a program to print reverse of a str if it is starting with digit """
+""" write a program to print reverse of a str if it is starting with digit """
 # s=input('enter a string:')
 # if s[0].isdigit():
 #     print(s[::-1])                 # by using inbuilt functions
@@ -68,26 +68,26 @@
 #     print("reverse the string starting with digit:",s[::-1])
 #   #output: 48 means 0 and 57 means 9 ascii values these are 48 and 57
 
-# """06-02-2026"""
-# """Write a program to print the uppercase letter and later convert into lower case letter"""
+"""06-02-2026"""
+"""Write a program to print the uppercase letter and later convert into lower case letter"""
 # char=input('enter the character:')
 # if  'A'<=char<='Z':
 #     print("the letter is in upper case:",ord(char))
 #     print("the conversion of upper case letter into lower case letter:",(chr(ord(char)+32)))
 #     print('the lower case number:',ord(chr(ord(char)+32)))        #  lower to  upper -32 and upper to lower +32
 
-# """Write  a program to check whether the data is single value data type"""
+"""Write  a program to check whether the data is single value data type"""
 # n=eval(input('enter the value:'))                                  # n=eval(input('enter any data'))
 # if type(n) not in (list,tuple,set,str,dict):              # (or)     if type(n) in (int,float,bool,complex):
 #     print('enter the values',type(n))                              # print('it is a single value datatype')
 
 
-# """Write  a program to check whether the data is mutable"""
+"""Write  a program to check whether the data is mutable"""
 # n=eval(input('enter the values'))
 # if type(n) in (list,set,dict):
 #     print('it is a mutable data types')
 
-# """ Write a program to print the given values are default values or not"""
+""" Write a program to print the given values are default values or not"""
 # n=eval(input('enter the data'))                                      data = eval(input('enter the value'))                  n=eval(input('enter the data'))
 # if  not bool(n):                                           (or)      if bool(data) is False:               (or)             if bool(n) == False:              (or) if bool(n) not in [True]:
 #     print(' default values',bool(type(n)))                            print('default value' )                               print('default value')
--- loopingstatements.py/whileloop.py
+++ loopingstatements.py/whileloop.py
@@ -1,3 +1,19 @@
+# Step 1️ Identify start value
+# Numbers → i = 1
+# String → i = 0
+# List → i = 0
+
+# Step 2️ Write condition
+# Numbers → i <= n
+# String → i < len(str)
+# List → i < len(list)
+
+# Step 3️ Write logic inside loop
+# print / sum / check / multiply
+
+# Step 4️ Increment
+# i += 1
+
 """Write a program to print 10 to 1 numbers"""
 # i=10
 # while i>=1:
@@ -29,3 +45,79 @@
 #     if n%i==0:
 #       print(i)
 #     i+=1
+
+"""Write  a program to find the sum of n natural numbers"""
+# n=int(input('enter the number:'))
+# i=1
+# a=0
+# while i<=n:
+#     a+=i
+#     i+=1
+# print(a)
+
+"""Write  a program to find the sum of n natural numbers in each and every iteration"""
+# n=int(input('enter the number:'))
+# i=1
+# a=0
+# while i<=n:
+#     a+=i
+#     i+=1
+#     print(a)
+   
+"""Write a program to find the factorial of a number"""
+# n=int(input('enter the number:'))         n=int(input('enter the number:))
+# i=1                                       fact=1
+# a=1                                       while n>=1:
+# while i<=n:            (or)                       fact*=n:
+#     a*=i                                          n-=1
+#     i+=1                                  print(fact)
+# print(a)
+
+"""Write a  program to print all the characters from a string"""
+# n=input(enter string:')
+# i=0
+# while i<len(n):
+#     print(n[i])
+#     i+=1
+
+"""Write  a program to print the vowels present in a string """
+# n=input('enter the string:')
+# i=0
+# k=''
+# v=['a','e','i','o','u','A','E','I','O','U']
+# while i<len(n):
+#     if n[i] in v:
+#       k+=n[i]
+#     i+=1
+# print(k)
+
+"""Write a program to toggle the string"""
+# n = input('enter the string')
+# i = 0
+# k = ""
+# while i < len(n):
+#     if 'A' <= n[i] <= 'Z':
+#         k = k + chr(ord(n[i]) + 32)    
+#     elif 'a' <= n[i] <= 'z':
+#         k = k + chr(ord(n[i]) - 32)    
+#     elif '0' <= n[i] <= '9':
+#         k = k + n[i]
+#     else:
+#         k = k + n[i]
+#     i += 1
+
+# print(k)
+
+
+"""Write a program to extract special characters from a string"""
+s = input("Enter a string: ")
+i = 0
+special = ""
+while i < len(s):
+    ch = s[i]
+    if not (('A' <= ch <= 'Z') or ('a' <= ch <= 'z') or ('0' <= ch <= '9')):
+        special = special + ch
+    i += 1
+print("Special characters:", special)
+
+"""Write a program to extract the integers from the list"""
