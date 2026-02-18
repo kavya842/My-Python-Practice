db1339bab0c9174408a4f07a1e9daf9b84d44dbb
kavya842 <kavyavankayalapati9999@gmail.com>
2026-02-18 11:23:27 +0530

    Updated python programs

--- loopingstatements.py/whileloop.py
+++ loopingstatements.py/whileloop.py
@@ -121,12 +121,85 @@
 # print("Special characters:", special)
 
 """Write a program to extract the integers from the list"""
-lst=eval(input('enter the list'))
-i=0
-a=[]
-while i<len(lst):
-    if type(lst[i])==int:
-        a.append(lst[i])
-    i+=1
-print(a)
+# lst=eval(input('enter the list'))
+# i=0
+# a=[]
+# while i<len(lst):
+#     if type(lst[i])==int:
+#         a.append(lst[i])
+#     i+=1
+# print(a)
 
+# """write a program to check whether a number is perfect number or not """ perfect number means sum of the factors of the number itself
+# n=int(input('enter the number'))
+# i=1
+# a=0
+# while i<n:
+#      if n%i==0:
+#           a=a+i
+#      i=i+1
+# print(a)
+# if a==n:
+#      print("perfect number")
+# else:
+#      print("Not a perfect number")
+"""Write a program to reverse  a integer"""
+# n=int(input('enter the number:'))
+# rev=0
+# while n > 0 :
+#     lastdigit=n%10
+#     rev=rev*10+lastdigit
+#     n=n//10
+# print(rev)
+"""Write a program to reverse a string without slicing"""
+# str=eval(input('enter the string'))
+# i=len(str)-1
+# rev=""
+# while i>=0:  
+#     rev=rev+str[i]
+#     i=i-1
+# print(rev)
+
+#  (or)
+
+# a=input('enter the string')
+# b=0
+# rev=''
+# while b<len(a):
+#     rev=a[b]+rev
+#     i=i+1
+# print(rev)
+
+"""Write a  program to print sum of the digits of the number"""
+# n=int(input('enter the number:'))
+# a=0
+# while n>0:
+#     a+= n%10
+#     n= n//10
+# print(a)
+
+"""Write a program to print the number armstrong or not"""
+# n=int(input('enter the number:'))
+# temp=n
+# s=0
+# while n>0:
+#     d=n%10
+#     s=s+d**3
+#     n=n//10
+# if temp==s:
+#     print('it is a armstrong')
+# else:
+#     print('it is not a armstrong')
+
+"""Write a program to print a  number is palindrome or not"""
+n=int(input('enter the numebr:'))
+temp=n
+a=0
+while n>0:
+    d=n%10
+    a=a*10+d
+    n=n//10
+if temp==a:
+    print('it is a palindrome')
+else:
+    print('it is not a palindrome')
