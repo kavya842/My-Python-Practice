4f31d6403eb4db2c651742342d0f7dbc400918fe
kavya842 <kavyavankayalapati9999@gmail.com>
2026-02-19 21:37:29 +0530

    updated for loop

---
+++ loopingstatements.py/forloop.py
@@ -0,0 +1,27 @@
+"""Write a program to print numbers from 10 to 1"""
+# for i in range(10,0,-1):
+#     print(i)
+"""Write a program to print odd numbers"""
+# for i in range(1,20,2):
+#     print(i)
+"""write a program to print even numbers"""
+# for i in range(0,20,2):
+#     print(i)
+"""Write a program to print characters present at even index"""
+# ch=eval(input())
+# for i in range(len(ch)):
+#     if i%2==0:
+#      print(ch[i])
+# (or)
+# ch=eval(input())
+# for i in range(0,len(ch),2):
+#     print(ch[i])
+"""Write a program to find the length of the collection without using len()"""
+# ch=eval(input())
+# count=0
+# for i in ch:
+#     count+=1
+# print(count)
+"""Assignment Questions"""
+"""Write a program the lowercase char if its ascii value is even"""
+"""Write  a program the sum of integers in present a  set """
--- loopingstatements.py/whileloop.py
+++ loopingstatements.py/whileloop.py
@@ -178,7 +178,7 @@
 #     n= n//10
 # print(a)
 
-"""Write a program to print the number armstrong or not"""
+"""Write a program to print whether the number armstrong or not"""
 # n=int(input('enter the number:'))
 # temp=n
 # s=0
@@ -190,16 +190,101 @@
 #     print('it is a armstrong')
 # else:
 #     print('it is not a armstrong')
+# (or)
+# n=int(input('enter the number:'))
+# count=0
+# sum=0
+# temp=n
+# temp1=n
+# while temp>0:
+#     count+=1
+#     temp=temp//10
+# while temp1>0:
+#     k=temp%10
+#     sum+=k**count
+#     temp1=temp1//10
+# if n==sum:
+#     print(f'it is a armstrong number')
+# else:
+#     print(f'it is a armstrong number')
+
 
 """Write a program to print a  number is palindrome or not"""
-n=int(input('enter the numebr:'))
-temp=n
-a=0
-while n>0:
-    d=n%10
-    a=a*10+d
-    n=n//10
-if temp==a:
-    print('it is a palindrome')
-else:
-    print('it is not a palindrome')
+# n=int(input('enter the numebr:'))
+# temp=n
+# a=0
+# while n>0:
+#     d=n%10
+#     a=a*10+d
+#     n=n//10
+# if temp==a:
+#     print('it is a palindrome')
+# else:
+#     print('it is not a palindrome')
+"""Write a program to count the no.of digits present in a number without using len()"""
+# n=int(input('enter the number:'))
+# i=0
+# while n>0:
+#     n%10
+#     n=n//10
+#     i+=1
+# print(i)
+# (or)
+# a=int(input('enter the number:'))
+# count=0
+# while a>0:
+#     count+=1
+#     a=a//10
+# print(count)
+"""Write a program to remove the duplicates from a list"""
+# l=eval(input())
+# i=0
+# lst1=[]
+# while i<len(l):
+#    if l[i] not in lst1:
+#       lst1+=[l[i]]
+#    i+=1
+# print(lst1)
+"""Write a program to find the greatest number from the list"""
+# n=eval(input())
+# i=0
+# n1=[0,]
+# while i<len(n):
+#     if n[i]>n1[0]:
+#         n1.pop()
+#         n1+=[n[i]]
+#     i+=1
+# print(n1)
+
+# (or)
+# n=eval(input())
+# i=0
+# largest=0
+# while i<len(n):
+#     if n[i]>largest:
+#         largest=n[i]
+#     i+=1
+# print(largest)
+
+"""Write a program to check whether the number is prime number or not"""
+# n = int(input())
+# i = 2
+# while i < n and n % i != 0:
+#     i += 1
+# if i == n and n > 1:
+#     print("prime")
+# else:
+#     print("not prime")
+"""Write a program to replace the space in a str with ** """ 
+# i/p:hello world
+# o/p:hello**world
+s=eval(input('enter the string'))
+o=" "
+i=0
+while i<len(s):
+    if s[i]==" ":
+        o=o+"**"
+    else:
+        o=o+s[i]
+    i+=1
+print(o)
