2abd901e67854b5b1313e5b7a23613d13bced675
kavya842 <kavyavankayalapati9999@gmail.com>
2026-02-19 22:09:48 +0530

    update for loop

--- loopingstatements.py/forloop.py
+++ loopingstatements.py/forloop.py
@@ -24,9 +24,14 @@
 # print(count)
 """Assignment Questions"""
 """Write a program the lowercase char if its ascii value is even"""
-s=eval(input())
-for i in s:
-    if i.islower():
-        if ord(i)%2==0:
-            print(i)
+# s=eval(input())
+# for i in s:
+#     if i.islower():
+#         if ord(i)%2==0:
+#             print(i)
 """Write  a program the sum of integers in present a  set """
+s=eval(input())
+sum=0
+for i in s:
+    sum=sum+i
+print(sum)
