df5d23ef0d3274ecf62d11bef08f28e3a7727013
kavya842 <kavyavankayalapati9999@gmail.com>
2026-03-04 13:13:35 +0530

    update functions

---
+++ functions/without arguements & without return function.py
@@ -0,0 +1,36 @@
+"""Write a program with using without arguements and without return statement"""
+# def rev():
+#     s=eval(input('enter the collection:'))
+#     if type(s) in [str,list,tuple]:
+#         print(s[::-1])
+#     else:
+#         print('enter the collection which supports indexing')
+# rev()
+
+"""Write a program to count the no.of uppercase char and no.of lowercase char and special char in a str"""
+# def count():
+#     s=eval(input('enter the string:'))
+#     spe=0
+#     upper=0
+#     lower=0
+#     for i in s:
+#         if i.isupper():
+#             upper+=1
+#         elif i.islower():
+#             lower+=1
+#         elif i.isalpha():
+#             spe+=1
+    # print(spe)
+#     print(upper)
+#     print(lower)
+# count()
+"""i/p:- 'hum apke hai kon' o/p:- 'kon hai apke hum' """
+# def rev():
+#     n=eval(input('enter the string:')).split()
+#     a=''
+#     b=" ".join(n[::-1])
+#     print(f"'{b}'")
+# rev()
+"""Write a function to concatinate two lists  without + operator """
+
+    
--- intermediate terminate statements/continue.py
+++ intermediate terminate statements/continue.py
@@ -33,24 +33,53 @@
 #         print(i)
 #         break
 """Write  a program to validate the password if the password is contain the 3 special characterrs and 2 numbers and 3 capital letters and password should be less than or equal to 8 and password is combination of all of these"""
-password=input('enter the password:')
-if len(password)>8:
-   s=0
-   c=0
-   d=0
-   for i in password:
-      if i.isupper():
-         c+=1
-      if i.isdigit():
-         d+=1
-      if i in '@#$%^&*':
-         s+=1
-   if s>=3 and c>=3 and d>=2:
-        print('hey your password is valid')
-   else:
-        print('hey your password is invalid')
-else:
-    print('length is less than 8')
+# password=input('enter the password:')
+# if len(password)>8:
+#    s=0
+#    c=0
+#    d=0
+#    for i in password:
+#       if i.isupper():
+#          c+=1
+#       if i.isdigit():
+#          d+=1
+#       if i in '@#$%^&*':
+#          s+=1
+#    if s>3 and c>=3 and d>2:
+#         print('hey your password is valid')
+#    else:
+#         print('hey your password is invalid')
+# else:
+#     print('length is less than 8')
+"""Write a program to print n number of prime numbers"""
+n=int(input('enter the number:'))
+count=0
+i=2
+while count<n:
+    for j in range(2,i):
+        if i%j==0:
+            break
+    else:
+        print(i)
+        count+=1
+    i+=1
+
+# (or in continue keyword)
+
+# n=int(input('enter the number:'))
+# count=0
+# i=2
+# while count<n:
+#     for j in range(2,i):
+#         if i%j==0:
+#             i+=1
+#             continue
+#     else:
+#         print(i)
+#         count+=1
+#     i+=1
+
+
           
     
 
