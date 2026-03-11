67734f995e4758e5d43f00857f328dea1dbe2ed2
kavya842 <kavyavankayalapati9999@gmail.com>
2026-03-11 12:53:58 +0530

    update types of arguements

---
+++ types of arguements/variable length arguements.py
@@ -0,0 +1,8 @@
+"""Packing for single or positional arguements """
+# def packing(*a, eid, eaddress):
+#     print(f'ename:{a}, eid:{eid}, eaddress:{eaddress}')
+# packing("kavya", eid=1260,eaddress= "kamepalli")
+
+# def test(*a, b):
+#     print(a, b)
+# test(10,20,30, b=40)
