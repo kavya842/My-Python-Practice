d5e284766c2a94b2e90df852a1b3a79e94b67803
kavya842 <kavyavankayalapati9999@gmail.com>
2026-03-18 21:06:16 +0530

    update oops

--- oops/class&object.py
+++ oops/class&object.py
@@ -42,24 +42,51 @@
 # print(student2.name,student2.fathername,student2.phno,student2.address,student2.course)
 
 """user defined method"""
-class institute:
-    institutename="jspiders"
-    address="hydjntu"
-    founder="kavya"
-    contactno=9878786543
-    def __init__(self,name,fathername,phno,address,course):
+# class institute:
+#     institutename="jspiders"
+#     address="hydjntu"
+#     founder="kavya"
+#     contactno=9878786543
+#     def __init__(self,name,fathername,phno,address,course):
+#         self.name=name
+#         self.fathername=fathername
+#         self.phno=phno
+#         self.address=address
+#         self.course=course
+#     def disp(self):
+#         print(self.name,self.fathername,self.phno,self.address,self.course)
+# student1=institute("kavya","Ravikumar",8919951981,"ongole","pythonfullstack")
+# student2=institute("mani","Ravikumar",8919951976,"ongole","javafullstack")
+# student1.disp()
+# student2.disp()
+# print(vars(student1))
+# print(vars(student2))
+# print(dir(institute))
+
+"""Modify two values of an object"""
+class panchayat:
+    wardno=10
+    panchayatname="kamepalli"
+    panchayatloc="kamepalli"
+    def __init__(self,name,fathername,village,panchayatname,wardno):
         self.name=name
         self.fathername=fathername
-        self.phno=phno
-        self.address=address
-        self.course=course
+        self.village=village
+        self.panchayatname=panchayatname
+        self.wardno=wardno
     def disp(self):
-        print(self.name,self.fathername,self.phno,self.address,self.course)
-student1=institute("kavya","Ravikumar",8919951981,"ongole","pythonfullstack")
-student2=institute("mani","Ravikumar",8919951976,"ongole","javafullstack")
-student1.disp()
-student2.disp()
-print(vars(student1))
-print(vars(student2))
+        print(self.name,self.fathername,self.village,self.panchayatname,self.wardno)
+    def old(self,addr):
+        self.village=addr
+    def new(self,addr):
+        self.village=addr
+people1=panchayat("kavya",  "ravikumar",   "kamepalli",  "kamepalli", 10)
+people2=panchayat("mani",    "ravi",        "kamepalli",  "kamepalli", 12)
+people1.disp() 
+people2.disp()
+people1.old('miyapur')
+people1.disp()
 
-        
+# print(vars(people1))
+# print(vars(people2))
+# print(dir(panchayat))
---
+++ oops/class&object1.py
@@ -0,0 +1,42 @@
+class school:
+    schoolname="CBSE"
+    address="hyderabad"
+    regNo=502316
+    schoolprinciple="kaveen"
+
+class1=school()
+class1.name="Amith"
+class1.fathername="Dinesh"
+class1.section='A'
+class1.phonenumber=7575654891
+
+class2=school()
+class2.name="Balu"
+class2.fathername="balaji"
+class2.section="B"
+class2.phonenumber=85474587861
+
+class3=school()
+class3.name="Sunny"
+class3.fathername="Rajasekhar"
+class3.section="A"
+class3.phonenumber=7418529630
+
+class4=school()
+class4.name="Mani"
+class4.fathername="Ravikumar"
+class4.section="c"
+class4.phonenumber=85556694561
+
+class5=school()
+class5.name="Srujanaa"
+class5.fathername="venkateswarluu"
+class5.section="A"
+class5.phonenumber=7533574125
+
+print(f"schoolname:{school.schoolname},schooladdress:{school.address},schoolregno:{school.regNo},principle:{school.schoolprinciple}")
+print(f"name:{class1.name}, fathername:{class1.fathername}, section:{class1.section}, phonenumber:{class1.phonenumber}")
+print(f"name:{class2.name}, fathername:{class2.fathername}, section:{class2.section}, phonenumber:{class2.phonenumber}")
+print(f"name:{class3.name}, fathername:{class3.fathername}, section:{class3.section}, phonenumber:{class3.phonenumber}")
+print(f"name:{class4.name}, fathername:{class4.fathername}, section:{class4.section}, phonenumber:{class4.phonenumber}")
+print(f"name:{class5.name}, fathername:{class5.fathername}, section:{class5.section}, phonenumber:{class5.phonenumber}")
