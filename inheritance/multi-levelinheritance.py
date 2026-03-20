f9adf20726f946e73a563930e6567cc00aa84e80
kavya842 <kavyavankayalapati9999@gmail.com>
2026-03-20 21:14:43 +0530

    update inheritance

--- inheritance/multi-levelinheritance.py
+++ inheritance/multi-levelinheritance.py
@@ -1,7 +1,4 @@
 class Whatsapp:
-    phcall="voicecall"
-    messages="whatsapp messages"
-    camera="photos"
     def __init__(self,messages,voicecall,videocall,status):
         self.messages=messages
         self.voicecall=voicecall
@@ -9,14 +6,25 @@ class Whatsapp:
         self.status=status
     def disp(self):
         print(self.messages,self.voicecall,self.videocall,self.status)
+
 class Update_whatsapp(Whatsapp):
-    def __init__(self,messages,voicecall,videocall,status,songforstatus,screenshare,onetapimagesorviedos):
+    def __init__(self,messages,voicecall,videocall,status,songforstatus,screenshare,onetapimages):
         super(). __init__(messages,voicecall,videocall,status)
         self.songforstatus=songforstatus
         self.screenshare=screenshare
-        self.onetapimagesorviedos=onetapimagesorviedos
+        self.onetapimages=onetapimages
     def disp(self):
         super().disp()
-        print(self.songforstatus,self.screenshare,self.onetapimagesorviedos)
-Up=Update_whatsapp("end to end messages :","voice call:","viedocall:","images:","song for image:","sharing the screen:","sending images for once:")
+        print("Updated Features:")
+        print(self.songforstatus,self.screenshare,self.onetapimages)
+
+class Latest_whatsapp(Update_whatsapp):
+    def __init__(self,messages,voicecall,videocall,status,songforstatus,screenshare,onetapimages,ai_chat):
+        super().__init__(messages,voicecall,videocall,status,songforstatus,screenshare,onetapimages)
+        self.ai_chat = ai_chat
+    def disp(self):
+        super().disp()
+        print("Latest Features")
+        print(self.ai_chat)
+Up=Latest_whatsapp("end to end messages","voice call","viedocall","images","song for image","sharing the screen","sending images for once","ai_chat")
 Up.disp()
