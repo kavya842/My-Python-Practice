62a0bf6ca1b303c29cfce4b938cf205e69535507
kavya842 <kavyavankayalapati9999@gmail.com>
2026-03-23 21:53:28 +0530

    update inheritance

--- inheritance/mini project.py
+++ inheritance/mini project.py
@@ -1,47 +1,50 @@
-class PythonCourse:
-    python_students = []
+class Fullstack:
+    def __init__(self):
+        self.fs_students = []
+    def add_fs(self,*name):
+        self.fs_students.extend(name)
+    def display_FS(self):
+        print("Full stack students:")
+        print(self.fs_students)
+class Datascience:
+    def __init__(self):
+        self.ds_students = []
+    def add_ds(self,*name):
+        self.ds_students.extend(name)
+    def display_ds(self):
+        print("Datascience students:")
+        print(self.ds_students)
+class Datanalytics:
+    def __init__(self):
+        self.da_students = []
+    def add_da(self,*name):
+        self.da_students.extend(name)
+    def display_da(self):
+        print("Data analytics students:")
+        print(self.da_students)
+class Testing:
+    def __init__(self):
+        self.ts_students = []
+    def add_ts(self,*name):
+        self.ts_students.extend(name)
+    def display_ts(self):
+        print("Testing students:")
+        print(self.ts_students)
+class PythonCourse(Fullstack, Datascience, Datanalytics, Testing):
+    def __init__(self):
+        Fullstack.__init__(self)
+        Datascience.__init__(self)
+        Datanalytics.__init__(self)
+        Testing.__init__(self)
 
-    def __init__(self, name):
-        self.name = name
-    def add_python(self):
-        PythonCourse.python_students.append(self.name)
-
-
-class DataScienceCourse:
-    ds_students = []
-
-    def __init__(self, name):
-        self.name = name
-    def add_ds(self):
-        DataScienceCourse.ds_students.append(self.name)
-
-
-class WebTechCourse:
-    webtech_students = []
-
-    def __init__(self, name):
-        self.name = name
-    def add_webtech(self):
-        WebTechCourse.webtech_students.append(self.name)
-
-
-class Student(PythonCourse, DataScienceCourse, WebTechCourse):
-
-    def __init__(self, name):
-        PythonCourse.__init__(self, name)
-        DataScienceCourse.__init__(self, name)
-        WebTechCourse.__init__(self, name)
-
-
-s1 = Student("Kavya")
-s1.add_python()
-
-s2 = Student("Ramya")
-s2.add_ds()
-
-s3 = Student("Harshini")
-s3.add_webtech()
-
-print("Python Students:", PythonCourse.python_students)
-print("Datascience Students:", DataScienceCourse.ds_students)
-print("Webtech Students:", WebTechCourse.webtech_students)
+    def disp_python(self):
+        self.display_FS()
+        self.display_ds()
+        self.display_da()
+        self.display_ts()
+s1 = PythonCourse()
+s1.add_fs("dora","ramya","surya")
+s1.add_ds("sravani","sravan","stayam")
+s1.add_da("pavani","pravathi","devi")
+s1.add_ts("lucky","gagan","raju")
+s1.disp_python()
