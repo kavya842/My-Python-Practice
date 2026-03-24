0334113c5f02d76987732e26f6c058b72247032b
kavya842 <kavyavankayalapati9999@gmail.com>
2026-03-24 22:07:34 +0530

    update encapsulation

--- encapsulation/private_acess_specifier.py
+++ encapsulation/private_acess_specifier.py
@@ -1,20 +1,51 @@
-class A:
-    __a=10
-    __b=20
-    def __init__(self,c):
-        self.c=c
+# class A:
+#     __a=10
+#     __b=20
+#     def __init__(self,c):
+#         self.c=c
+#     def __disp(self):
+#         print(self.c)
+#     @classmethod
+#     def __display(cls):
+#         print(cls.__a,cls.__b)
+#     @staticmethod
+#     def __msg():
+#         print("public access specifier")
+# obj=A(60)
+# A.__a=90
+# A.__b=12
+# obj.__display()
+# obj.__disp()
+# A.__display()
+# obj.__msg()
+
+"""1)syntax method"""
+class School:
+    __Sname="sri chaitanya"
+    __Sloc="JNTU"
+    def __init__(self,name):
+        self.__name=name
     def __disp(self):
-        print(self.c)
-    @classmethod
-    def __display(cls):
-        print(cls.__a,cls.__b)
-    @staticmethod
-    def __msg():
-        print("public access specifier")
-obj=A(60)
-A.__a=90
-A.__b=12
-obj.__display()
-obj.__disp()
-A.__display()
-obj.__msg()
+        print(self.__name)
+s1=School('Dora')
+print(School._School__Sname)
+s1._School__disp()
+School._School__Sname="narayana"
+print(School._School__Sname)
+
+"""getter and setter function"""
+class School:
+    __Sname="Sri chaitanya"
+    __Sloc="jntu"
+    def __init__(self,name):
+        self.__name=name
+    def __disp(self):
+        print(self.__name)
+    def get_var(self):
+        print(self.__name)
+    def set_var(self,new):
+        self.__name=new
+S1=School("dora")
+S1.get_var()
+S1.set_var('bujji')
+S1.get_var()
