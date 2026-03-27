20d9c3887a219d7f61d7e43e5e3c6963e02f8ecc
kavya842 <kavyavankayalapati9999@gmail.com>
2026-03-27 13:13:34 +0530

    update comprehension

---
+++ comprehension/dictionary_comprehension.py
@@ -0,0 +1,8 @@
+# d={'ice cream':50,'chocolate':198,'biscuit':25,'cake':450,'lays':59}
+# print({i:d[i] for i in d if d[i]>100})
+# print({i:d[i] for i in d if d[i]<100})
+
+# s='sunday is working day'
+# """o/p:- {'sunday':'SUNDAY','is':'IS','working':'gnikrow,'day':'yad'}"""
+# print({i:i.upper() if len(i)%2==0 else i[::-1] for i in s.split()})
+
