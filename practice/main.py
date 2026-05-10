7a9d44426c01685db348a6f8f73ebe32653c395c
kavya842 <kavyavankayalapati9999@gmail.com>
2026-05-10 14:18:45 +0530

    update code

---
+++ practice/main.py
@@ -0,0 +1,12 @@
+"""Write a program to print a number if it is even"""
+# n=int(input('enter the number:'))
+# if n%2==0:
+#     print(n)
+"""Write a program to print the square of a number if number is even"""
+# n=int(input('enter the number:'))
+# if n%2==0:
+#     print(n**2)
+"""Write a program whether the character is vowel"""
+char=input('enter the character:')
+if char in ['a','e','i','o','u']:
+    print(char)
