181d8a5ee30557934329e036e4697f46dd5d0e2e
kavya842 <kavyavankayalapati9999@gmail.com>
2026-06-08 21:39:30 +0530

    update code

--- inheritance/mini project.py
+++ inheritance/mini project.py
@@ -1,11 +1,19 @@
-class Fullstack:
+class PythonFullstack:
     def __init__(self):
-        self.fs_students = []
-    def add_fs(self,*student):
-        self.fs_students.extend(student)
-    def display_FS(self):
-        print("Full stack students:")
-        print(self.fs_students)
+        self.pfs_students = []
+    def add_pfs(self,*student):
+        self.pfs_students.extend(student)
+    def display_PFS(self):
+        print("Python full stack students:")
+        print(self.pfs_students)
+class Javafullstack:
+    def __init__(self):
+        self.jfs_students=[]
+    def add_jfs(self,*student):
+        self.jfs_students.extend(student)
+    def display_jfs(self):
+        print("Java full stack students:")
+        print(self.jfs_students)
 class Datascience:
     def __init__(self):
         self.ds_students = []
@@ -30,20 +38,23 @@ class Testing:
     def display_ts(self):
         print("Testing students:")
         print(self.ts_students)
-class PythonCourse(Fullstack, Datascience, Datanalytics, Testing):
+class PythonCourse(PythonFullstack, Javafullstack, Datascience, Datanalytics, Testing):
     def __init__(self):
-        Fullstack.__init__(self)
+        PythonFullstack.__init__(self)
+        Javafullstack.__init__(self)
         Datascience.__init__(self)
         Datanalytics.__init__(self)
         Testing.__init__(self)
 
     def disp_python(self):
-        self.display_FS()
+        self.display_PFS()
+        self.display_jfs()
         self.display_ds()
         self.display_da()
         self.display_ts()
 s1 = PythonCourse()
-s1.add_fs("dora",8919951981,"ramya",9652738424,"surya",7671071426)
+s1.add_pfs("dora",8919951981,"ramya",9652738424,"surya",7671071426)
+s1.add_jfs("siva",1234567897,"ravi",9987654321)
 s1.add_ds("sravani","sravan","stayam")
 s1.add_da("pavani","pravathi","devi")
 s1.add_ts("lucky","gagan","raju")
--- practice/main.py
+++ practice/main.py
@@ -7,6 +7,25 @@
 # if n%2==0:
 #     print(n**2)
 """Write a program whether the character is vowel"""
-char=input('enter the character:')
-if char in ['a','e','i','o','u']:
-    print(char)
+# char=input('enter the character:')
+# if char in ['a','e','i','o','u']:
+#     print(char)
+"""Write a program to remove duplicates from a list using comprehension """
+# n=[10,20,30,40,50,10,20]
+# unique_list=list(set(n))
+# print(unique_list)
+"""Write a program to remove duplicates from a list using list comprehension without using set() function """
+# n=[10,20,30,40,10,20]
+# n1=[]
+# for i in n:
+#     if i not in n1:
+#         n1.append(i)
+# print(n1)
+"""i/p:-'aabcbba output:-a2b1c1b2a1"""
+s='aabcbba'
+result=''
+for i in s:
+    count=s.count(i)
+    if i not in result:
+        result+=i+str(count)
+print(result)
